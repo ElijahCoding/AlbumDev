71e7ee1e1a485c0999041bd2a4605a9a4a2a7cf8
ElijahCoding <cuiyeqing960904@gmail.com>
2019-02-05 19:03:09 +0300

    reddit transformer

---
+++ app/Services/Transformers/RedditTransformer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services\Transformers;
+
+use App\Services\Transformers\TransformerAbstract;
+
+class RedditTransformer extends TransformerAbstract
+{
+    public function transform($payload)
+    {
+        return [
+            'title' => $payload->data->title,
+            'link' => 'https://reddit.com' . $payload->data->permalink,
+            'timestamp' => $payload->data->created_utc
+        ];
+    }
+}
