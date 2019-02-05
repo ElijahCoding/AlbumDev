a42d13c002f2cd9f2cdb506ac8de79d512339dd3
ElijahCoding <cuiyeqing960904@gmail.com>
2019-02-05 19:42:23 +0300

    caching cdn libraries

--- app/Http/Controllers/Programming/CdnController.php
+++ app/Http/Controllers/Programming/CdnController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Programming;
 
 use Illuminate\Http\Request;
+use App\Services\CdnJS\Libraries;
 use App\Http\Controllers\Controller;
 
 class CdnController extends Controller
 {
     public function index()
     {
-        
+        return view('programming.cdn.index', [
+            'libraries' => Libraries::get()
+        ]);
     }
 }
--- app/Services/CdnJS/Libraries.php
+++ app/Services/CdnJS/Libraries.php
@@ -2,7 +2,17 @@
 
 namespace App\Services\CdnJS;
 
+use Cache;
+use App\Models\Programming\Cdn\Library;
+
 class Libraries
 {
-    
+    public static function get()
+    {
+        $libraries = Cache::remember('libraries', 30, function () {
+            return json_encode(Library::get());
+        });
+
+        return json_decode($libraries);
+    }
 }
