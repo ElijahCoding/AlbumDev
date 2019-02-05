90ed511d9e1d5590b9f91769095e3e72a291903a
ElijahCoding <cuiyeqing960904@gmail.com>
2019-02-05 08:37:28 +0300

    picture modal

--- resources/js/components/album/picture/Pictures.vue
+++ resources/js/components/album/picture/Pictures.vue
@@ -1,39 +1,18 @@
 <template>
     <div class="card">
         <div class="row">
-            <div class="col-md-3 mt-2" v-for="picture in pictures" data-toggle="modal" :data-target="'#picture-' + picture.id">
-                <div class="card">
+            <div class="col-md-3 mt-2" v-for="picture in pictures">
+                <div class="card" @click="showModal(picture.id)">
                     <img class="card-img-top"
                          :src="picture.file_path"
                          :alt="picture.name"
                          >
                 </div>
 
-                <div class="modal fade" :id="'picture-' + picture.id" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                  <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h5 class="modal-title" :id="'picture-' + picture.id">{{ picture.name }}</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-                      <div class="modal-body">
-                          <div class="container">
-                              <div class="row">
-                                  <img :src="picture.file_path"
-                                       :alt="picture.name"
-                                       >
-                              </div>
-                          </div>
-                      </div>
-                      <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
-                      </div>
-                    </div>
-                  </div>
-              </div>
+                <modal :name="'picture-' + picture.id">
+                    <p>{{ picture.name }}</p>
+                </modal>
+
             </div>
         </div>
     </div>
@@ -46,6 +25,12 @@
             pictures: {
                 required: true,
                 type: Array
+            }
+        },
+
+        methods: {
+            showModal (id) {
+                this.$modal.show(`picture-${id}`);
             }
         }
     }
--- resources/js/plugins.js
+++ resources/js/plugins.js
@@ -1,5 +1,5 @@
 import Notifications from 'vue-notification'
-// import VModal from 'vue-js-modal'
+import VModal from 'vue-js-modal'
 
 Vue.use(Notifications)
-// Vue.use(VModal)
+Vue.use(VModal)
