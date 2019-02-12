ace25cefd10fccb186ac9511c6deb30aa0112696
ElijahCoding <cuiyeqing960904@gmail.com>
2019-02-12 18:34:38 +0300

    library detail

--- public/js/app.js
+++ public/js/app.js
@@ -2049,9 +2049,47 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   props: ['library'],
-  created: function created() {}
+  data: function data() {
+    return {
+      moment: moment
+    };
+  },
+  created: function created() {
+    console.log(this.library);
+  }
 });
 
 /***/ }),
@@ -55684,7 +55722,69 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div")
+  return _c("div", { staticClass: "container" }, [
+    _c("div", { staticClass: "card text-center" }, [
+      _c("div", { staticClass: "card-header" }, [
+        _vm._v("\n          " + _vm._s(_vm.library.name) + "\n        ")
+      ]),
+      _vm._v(" "),
+      _c("div", { staticClass: "card-body" }, [
+        _c("h5", {
+          staticClass: "card-title",
+          domProps: { textContent: _vm._s(_vm.library.link) }
+        }),
+        _vm._v(" "),
+        _c("hr"),
+        _vm._v(" "),
+        _vm.library.library_detail.description
+          ? _c("p", {
+              staticClass: "card-text",
+              domProps: {
+                textContent: _vm._s(_vm.library.library_detail.description)
+              }
+            })
+          : _vm._e(),
+        _vm._v(" "),
+        _vm.library.library_detail.homepage
+          ? _c(
+              "a",
+              {
+                staticClass: "btn btn-primary",
+                attrs: {
+                  href: _vm.library.library_detail.homepage,
+                  target: "_blank"
+                }
+              },
+              [_vm._v("\n             Home Page\n         ")]
+            )
+          : _vm._e()
+      ]),
+      _vm._v(" "),
+      _vm.library.library_detail.repository
+        ? _c(
+            "a",
+            {
+              staticClass: "card-footer text-muted",
+              attrs: {
+                href: _vm.library.library_detail.repository,
+                target: "_blank"
+              }
+            },
+            [
+              _c("strong", { staticClass: "float-left" }, [
+                _vm._v("Repository")
+              ]),
+              _vm._v(" "),
+              _c("span", {
+                domProps: {
+                  textContent: _vm._s(_vm.library.library_detail.repository)
+                }
+              })
+            ]
+          )
+        : _vm._e()
+    ])
+  ])
 }
 var staticRenderFns = []
 render._withStripped = true
