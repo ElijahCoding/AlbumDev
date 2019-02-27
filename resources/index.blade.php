6ab392603b91eee243d5bffa0438e50192a391d7
ElijahCoding <cuiyeqing960904@gmail.com>
2019-02-27 22:38:08 +0300

    changing to spa

---
+++ resources/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Album</title>
+    </head>
+    <body>
+        <div id="app">
+            <App />
+        </div>
+    </body>
+</html>
