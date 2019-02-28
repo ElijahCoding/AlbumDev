8816aef8432be8f9e41b18eb4582415d9e68a174
ElijahCoding <cuiyeqing960904@gmail.com>
2019-02-28 14:53:18 +0300

    name removed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -8,7 +8,6 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         User::create([
-            'name' => 'elijah',
             'email' => 'stanislav960904@gmail.com',
             'password' => bcrypt(config('services.authentication.password'))
         ]);
